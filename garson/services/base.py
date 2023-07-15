b0f78afbea79c0fcbd749d444111b7fd60e73a8d
Dima Burmistrov <pyctrl.dev@gmail.com>
2023-07-15 18:52:46 +0000

    Update file base.py

--- garson/services/base.py
+++ garson/services/base.py
@@ -11,6 +11,7 @@ import weakref
 from garson._lib import constants as c
 from garson._lib import info as i
 from garson._lib import log
+from garson._lib import utils
 from garson.contexts import base as g_ctxs
 from garson.contexts import daemon as g_daemon
 
