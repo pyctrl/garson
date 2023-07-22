1f842689686d8c22e259e6f845616a626dc2c238
Dima Burmistrov <pyctrl.dev@gmail.com>
2023-07-22 16:12:36 +0000

    Update file fake.py

--- garson/services/fake.py
+++ garson/services/fake.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
 
 import contextlib
+import logging
 import signal
 
-from garson.services import base
 from garson.contexts import daemon as g_daemon
+from garson.services import base
 
 
 LOG = logging.getLogger(__name__)
 
 
-class FakeService(abc.ABC):
+class FakeService(base.AbstractService):
 
     SERVICE_TYPE = "fake"
 
